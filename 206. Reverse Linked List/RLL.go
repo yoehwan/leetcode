dfea42bfb81af4cc4b1489ff04315253447a9acd
yoehwan <yoehwan@gmail.com>
2020-08-08 21:04:16 +0800

    206

--- 206. Reverse Linked List/RLL.go
+++ 206. Reverse Linked List/RLL.go
@@ -5,20 +5,37 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func reverseList(head *ListNode) *ListNode {
+// func reverseList(head *ListNode) *ListNode {
 
-	var previous *ListNode
-	current := head
-	for current != nil {
-		nextTemp := current.Next
-		current.Next = previous
-		previous = current
-		current = nextTemp
+// 	var previous *ListNode
+// 	current := head
+// 	for current != nil {
+// 		nextTemp := current.Next
+// 		current.Next = previous
+// 		previous = current
+// 		current = nextTemp
+// 	}
+
+// 	return previous
+
+// }
+
+
+
+func reverseList(head *ListNode) *ListNode {
+	var prev, next *ListNode
+	curr := head
+
+	for ;curr!=nil; {
+		next = curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
 	}
 
-	return previous
-}
+	return prev
 
+}
 func main() {
 	input := ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}
 
